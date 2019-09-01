3af973878e5475819acbd088146c68b621bfb5b5
Raviraj2277 <ravirajhirve@gmail.com>
2019-09-02 03:49:39 +0530

    nav

--- script.js
+++ script.js
@@ -1,75 +1,18 @@
-/*global $, document, setTimeout, i*/
-$(document).ready(function () {
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
 
-    'use strict';
+navbarToggler.addEventListener("click", navbarTogglerClick);
 
-    var slide  = $('.slide'),
-        slideAelements = $('.slide-a-child'),
-        slideBelements = $('.slide-b-child'),
-        slideCelements = $('.slide-c-child'),
-        slideDelements = $('.slide-d-child'),
-        replay = $('button.replay');
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
 
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
 
-
-    slide.each(function (i) {
-        if (i < 3) {
-            setTimeout(function () {
-                slide.eq(i).fadeOut();
-            }, 5000 * (i + 1));
-        }
-    });
-
-
-    function animateSlideA() {
-      slideAelements.each(function (i) {
-          setTimeout(function () {
-              slideAelements.eq(i).addClass('is-visible');
-          }, 300 * (i + 1));
-      });
-    }
-
-    function animateSlideB() {
-      slideBelements.each(function (i) {
-          setTimeout(function () {
-              slideBelements.eq(i).addClass('is-visible');
-          }, 300 * (i + 1));
-      });
-   }
-
-     function animateSlideC() {
-      slideCelements.each(function (i) {
-          setTimeout(function () {
-              slideCelements.eq(i).addClass('is-visible');
-          }, 150 * (i + 1));
-      });
-    }
-
-    function animateSlideD() {
-      slideDelements.each(function (i) {
-          setTimeout(function () {
-              slideDelements.eq(i).addClass('is-visible');
-          }, 300 * (i + 1));
-      });
-    }
-
-    animateSlideA();
-
-    setTimeout(function () {
-       animateSlideB();
-    }, 5000);
-
-    setTimeout(function () {
-       animateSlideC();
-    }, 10000);
-
-    setTimeout(function () {
-       animateSlideD();
-    }, 15000);
-
-
-   replay.on('click', function () {
-     location.reload(true);
-   });
-
-});
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
